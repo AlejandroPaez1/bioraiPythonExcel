4567fa50afe78e0f2dcb4c8fd3d66a01603a6df4
AlejandroPaez1 <alejandro334paez@gmail.com>
2024-05-17 17:57:10 -0600

    se cambio el fomrato de impresion las checadas tienen las horas en que checo

--- Validar2v.py
+++ Validar2v.py
@@ -37,68 +37,36 @@ def buscar_dias_checados_por_empleado(archivo_checadores):
 
 # Llamada a la función para buscar los días checados por empleado
 checadas_por_empleado_y_fecha, data_checadores = buscar_dias_checados_por_empleado("archivo.csv")
-def buscar_nombre_por_pin(pin):
-    nombre_empleado = None
-    for empleado in data_checadores:
-        if empleado['PIN'] == pin:
-            nombre_empleado = empleado['Nombre de empleado']
-            break  # Si encuentras el empleado, puedes salir del bucle
-    return nombre_empleado
 
-def buscar_dispositivo_por_pin(pin):
-    nombre_empleado = None
+def buscar_nombre_dispositivo_por_pin(pin):
     for empleado in data_checadores:
         if empleado['PIN'] == pin:
-            nombre_empleado = empleado['Dispositivo']
-            break  # Si encuentras el empleado, puedes salir del bucle
-    return nombre_empleado
+            return empleado['Nombre de empleado'], empleado['Dispositivo']
+    return None, None
 
 # Crear un nuevo archivo de Excel
 wb = openpyxl.Workbook()
 ws = wb.active
 
 # Escribir encabezados al archivo de Excel
-# Escribir datos en el archivo de Excel
-# Obtener todas las fechas únicas de las checadas
-fechas = sorted(set(checada for checadas_por_fecha in checadas_por_empleado_y_fecha.values() for checada in checadas_por_fecha.keys()))
-
-# Escribir encabezados al archivo de Excel
-encabezados = ["PIN", "Nombre", "Dispositivo"]
-encabezados.extend([fecha.strftime("%d/%m/%Y") for fecha in fechas])
+encabezados = ["PIN", "NOMBRE", "DISPOSITIVO", "Fecha"]
 ws.append(encabezados)
 # Escribir datos en el archivo de Excel
 for pin, checadas_por_fecha in checadas_por_empleado_y_fecha.items():
-    nombre_empleado = None
-    dispositivo = None
-    for fecha in fechas:
-        if fecha in checadas_por_fecha:
-            checadas = checadas_por_fecha[fecha]
-            if not nombre_empleado:
-                nombre_empleado = buscar_nombre_por_pin(pin)
-                dispositivo = buscar_dispositivo_por_pin(pin)
-            # Obtener todas las horas únicas de checadas en el día
-            horas = sorted(set(checada.hour for checada in checadas))
-            # Iterar sobre las horas de checada
-            for hora in horas:
-                # Filtrar las checadas de esta hora
-                checadas_en_hora = [checada for checada in checadas if checada.hour == hora]
-                # Dividir las checadas en dos grupos
-                num_checadas = len(checadas_en_hora)
-                group_size = num_checadas // 2
-                grupo_1 = checadas_en_hora[:group_size]
-                grupo_2 = checadas_en_hora[group_size:]
-                # Escribir fila para el primer grupo de checadas
-                for checada in sorted(grupo_1):
-                    fila = [int(pin), nombre_empleado, dispositivo] + [""] * len(fechas)
-                    fila[encabezados.index(fecha.strftime("%d/%m/%Y"))] = checada.strftime("%H:%M:%S")
-                    ws.append(fila)
-                # Escribir fila para el segundo grupo de checadas
-                for checada in sorted(grupo_2):
-                    fila = [int(pin), nombre_empleado, dispositivo] + [""] * len(fechas)
-                    fila[encabezados.index(fecha.strftime("%d/%m/%Y"))] = checada.strftime("%H:%M:%S")
-                    ws.append(fila)
+    nombre_empleado, dispositivo = buscar_nombre_dispositivo_por_pin(pin)
+    for fecha, checadas in checadas_por_fecha.items():
+        # Ordenar las checadas por hora
+        checadas.sort()
+        # Escribir la información en el archivo Excel
+        fila = [int(pin), nombre_empleado, dispositivo, fecha.strftime("%d/%m/%Y")]
+        # Agregar las horas de checada en una sola cadena separada por espacio
+        # horas_checadas = (checada.strftime("%H:%M:%S") for checada in checadas)
+        # fila.append( (checada.strftime("%H:%M:%S") for checada in checadas))
+        fila.extend(checada.strftime("%H:%M:%S") for checada in checadas)
+
+        ws.append(fila)
 
 
 # Guardar el archivo de Excel
-nombre_archivo = "test223sintiuuupo.xlsx"
+nombre_archivo = "test223sintiuuupov4.xlsx"
 wb.save(nombre_archivo)
--- validarFaltasSemanalCSV.py
+++ validarFaltasSemanalCSV.py
@@ -1,187 +1,137 @@
 import csv
 import os
-from tkinter import Tk, filedialog,messagebox
+from tkinter import Tk, filedialog, messagebox
 from datetime import datetime, timedelta
 import openpyxl
 from collections import defaultdict
-try:
-    def determinar_formato(checada_str):
-        if '/' in checada_str:
-            if len(checada_str) == 16:
-                return "%d/%m/%Y %H:%M"
-            elif len(checada_str) == 19:
-                return "%d/%m/%Y %H:%M:%S"
-        elif '-' in checada_str:
-            if len(checada_str) == 16:
-                return "%d-%m-%Y %H:%M"
-            elif len(checada_str) == 19:
-                return "%d-%m-%Y %H:%M:%S"
-        else:
-            messagebox.showinfo("Formato de fecha y hora no reconocido")
-            # print("Formato de fecha y hora no reconocido")
-            raise ValueError("Formato de fecha y hora no reconocido")
-        
-    def obtener_nombre_archivo(nombre_base, extension):
-        contador = 1
-        while True:
-            nombre_archivo = f"{nombre_base}_({contador}).{extension}"
-            if not os.path.exists(nombre_archivo):
-                return nombre_archivo
-            contador += 1
+import re
 
-    def buscar_no_checadores(archivo_checadores):
-        with open(archivo_checadores, 'r', newline='', encoding='utf-8') as file:
-            csv_reader = csv.DictReader(file)
-            data_checadores = list(csv_reader)
+def determinar_formato(checada_str):
+    if '/' in checada_str:
+        if len(checada_str) == 16:
+            return "%d/%m/%Y %H:%M"
+        elif len(checada_str) == 19:
+            return "%d/%m/%Y %H:%M:%S"
+    elif '-' in checada_str:
+        if len(checada_str) == 16:
+            return "%d-%m-%Y %H:%M"
+        elif len(checada_str) == 19:
+            return "%d-%m-%Y %H:%M:%S"
+    raise ValueError("Formato de fecha y hora no reconocido")
 
-        # Crear un diccionario para almacenar las fechas de las checadas por PIN
-        checadas_por_pin = defaultdict(list)
-        for empleado in data_checadores:
-            pin = empleado["PIN"]
-            nombre = empleado["Nombre de empleado"]
-            dispositivo = empleado["Dispositivo"]
-            checada_str = empleado["Checada"]
-            # Convertir la cadena de fecha y hora en un objeto datetime
-            formato = determinar_formato(checada_str)
-            checada_dt = datetime.strptime(checada_str, formato)
-            # Almacenar la fecha de la checada en el diccionario
-            checadas_por_pin[pin].append((checada_dt, nombre, dispositivo))
-        # Obtener la fecha mínima global y máxima global de todas las checadas
-        todas_las_fechas = [checada.date() for checadas in checadas_por_pin.values() for checada, _, _ in checadas]
-        primer_dia_global = min(todas_las_fechas)
-        ultimo_dia_global = max(todas_las_fechas)
+def obtener_nombre_archivo(nombre_base, extension):
+    contador = 1
+    while True:
+        nombre_archivo = f"{nombre_base}_({contador}).{extension}"
+        if not os.path.exists(nombre_archivo):
+            return nombre_archivo
+        contador += 1
 
-        # Crear una lista con todas las fechas dentro del rango de fechas
-        todas_las_fechas_posibles = [primer_dia_global + timedelta(days=i) for i in range((ultimo_dia_global - primer_dia_global).days + 1)]
-        # Crear un diccionario para almacenar los días en que no se checó por PIN
-        dias_no_checados_por_pin = defaultdict(list)
+def buscar_no_checadores(archivo_checadores):
+    with open(archivo_checadores, 'r', newline='', encoding='utf-8') as file:
+        csv_reader = csv.DictReader(file)
+        data_checadores = list(csv_reader)
 
-        for pin, checadas in checadas_por_pin.items():
-            # Obtener el conjunto único de fechas (días) en que se checó para este PIN
-            dias_checados = set(checada.date() for checada, _, _ in checadas)
-            # Construir días no checados por empleado
-            for fecha in todas_las_fechas_posibles:
-                if fecha not in dias_checados:
-                    # Verificar si el PIN ya tiene una entrada en el diccionario
-                    if pin in dias_no_checados_por_pin:
-                        # Si ya tiene una entrada, agregar la fecha no checada, nombre y dispositivo a las listas existentes
-                        dias_no_checados_por_pin[pin][0].append(fecha)
-                    else:
-                        # Si no tiene una entrada, crear una nueva lista para el PIN
-                        dias_no_checados_por_pin[pin] = ([fecha], [nombre], [dispositivo])
+    checadas_por_pin = defaultdict(list)
+    for empleado in data_checadores:
+        pin = empleado["PIN"]
+        nombre = empleado["Nombre de empleado"]
+        dispositivo = empleado["Dispositivo"]
+        checada_str = empleado["Checada"]
+        formato = determinar_formato(checada_str)
+        checada_dt = datetime.strptime(checada_str, formato)
+        checadas_por_pin[pin].append((checada_dt, nombre, dispositivo))
 
-        return dias_no_checados_por_pin, data_checadores
+    todas_las_fechas = [checada.date() for checadas in checadas_por_pin.values() for checada, _, _ in checadas]
+    primer_dia_global = min(todas_las_fechas)
+    ultimo_dia_global = max(todas_las_fechas)
 
-    # Crear ventana de Tkinter para seleccionar el archivo
+    todas_las_fechas_posibles = [primer_dia_global + timedelta(days=i) for i in range((ultimo_dia_global - primer_dia_global).days + 1)]
+    dias_no_checados_por_pin = defaultdict(list)
+
+    for pin, checadas in checadas_por_pin.items():
+        dias_checados = set(checada.date() for checada, _, _ in checadas)
+        for fecha in todas_las_fechas_posibles:
+            if fecha not in dias_checados:
+                dias_no_checados_por_pin[pin].append(fecha)
+
+    return dias_no_checados_por_pin, data_checadores
+
+def extraer_numeros_checada(celda):
+    patron_horaminuto = r'\b\d{2}:\d{2}\b'
+    horas_minutos_encontrados = re.findall(patron_horaminuto, celda)
+    if horas_minutos_encontrados:
+        return horas_minutos_encontrados
+    return None
+
+def obtener_numeros_checada_por_pin_y_fecha(pin, fecha, data_checadores):
+    numeros_checada = []
+    fecha_str = fecha.strftime("%d-%m-%Y")
+    for empleado in data_checadores:
+        if empleado['PIN'] == pin and empleado['Checada'].startswith(fecha_str):
+            numeros_checada.extend(extraer_numeros_checada(empleado['Checada']))
+    return numeros_checada
+
+def buscar_nombre_por_pin(pin, data_checadores):
+    for empleado in data_checadores:
+        if empleado['PIN'] == pin:
+            return empleado['Nombre de empleado']
+    return None
+
+def buscar_dispositivo_por_pin(pin, data_checadores):
+    for empleado in data_checadores:
+        if empleado['PIN'] == pin:
+            return empleado['Dispositivo']
+    return None
+
+def main():
     root = Tk()
-    root.withdraw()  # Ocultar la ventana principal
+    root.withdraw()
 
-    # Ventana emergente para explicar el rango de fechas esperado
-    messagebox.showinfo("Rango de fechas",
-                        "Por favor, asegúrese de seleccionar un archivo CSV que contenga registros de checadas de hasta una semana atrás hasta la fecha actual.")
+    messagebox.showinfo("Rango de fechas", "Por favor, asegúrese de seleccionar un archivo CSV que contenga registros de checadas de hasta una semana atrás hasta la fecha actual.")
 
-    # Solicitar al usuario que seleccione el archivo CSV
     archivo_checadores = filedialog.askopenfilename(title="Seleccione el archivo CSV", filetypes=[("Archivos CSV", "*.csv")])
 
-    # Solicitar al usuario que seleccione el archivo CSV
-    # archivo_checadores = filedialog.askopenfilename(title="Seleccione el archivo CSV", filetypes=[("Archivos CSV", "*.csv")])
+    try:
+        dias_no_checados_por_empleado, data_checadores = buscar_no_checadores(archivo_checadores)
+        
+        wb = openpyxl.Workbook()
+        ws = wb.active
 
-    # Buscar días en que no se checó por empleado
-    dias_no_checados_por_empleado, data_checadores = buscar_no_checadores(archivo_checadores)
+        ws['A1'] = "PIN"
+        ws['B1'] = "Nombre"
+        ws['C1'] = "Dispositivo"
 
-    # Crear un nuevo archivo de Excel
-    wb = openpyxl.Workbook()
-    ws = wb.active
+        fechas = sorted(set(fecha for dias_no_checados in dias_no_checados_por_empleado.values() for fecha in dias_no_checados))
 
-    # Encabezados
-    ws['A1'] = "PIN"
-    ws['B1'] = "Nombre"
-    ws['C1'] = "Dispositivo"
-    # Escribir encabezados de fechas dinámicamente
-    fechas = sorted(set(fecha for dias_no_checados, _, _ in dias_no_checados_por_empleado.values() for fecha in dias_no_checados))
+        for idx, fecha in enumerate(fechas, start=1):
+            ws.cell(row=1, column=3+idx).value = fecha.strftime("%d/%m/%Y")
 
-    for idx, fecha in enumerate(fechas, start=1):
-        ws.cell(row=1, column=3+idx).value = f"{fecha}"
+        row = 2
+        for pin, dias_no_checados in dias_no_checados_por_empleado.items():
+            if dias_no_checados:
+                ws.cell(row=row, column=1).value = int(pin)
+                ws.cell(row=row, column=2).value = buscar_nombre_por_pin(pin, data_checadores)
+                ws.cell(row=row, column=3).value = buscar_dispositivo_por_pin(pin, data_checadores)
 
-    def buscar_nombre_por_pin(pin):
-        nombre_empleado = None
-        for empleado in data_checadores:
-            if empleado['PIN'] == pin:
-                nombre_empleado = empleado['Nombre de empleado']
-                break  # Si encuentras el empleado, puedes salir del bucle
-        return nombre_empleado
+                for idx, fecha in enumerate(fechas, start=1):
+                    if fecha in dias_no_checados:
+                        ws.cell(row=row, column=3+idx).value = "F"
+                    else:
+                        numeros_checada = obtener_numeros_checada_por_pin_y_fecha(pin, fecha, data_checadores)
+                        if numeros_checada:
+                            ws.cell(row=row, column=3+idx).value = ', '.join(numeros_checada)
+                row += 1
 
-    def buscar_dispositivo_por_pin(pin):
-        nombre_empleado = None
-        for empleado in data_checadores:
-            if empleado['PIN'] == pin:
-                nombre_empleado = empleado['Dispositivo']
-                break  # Si encuentras el empleado, puedes salir del bucle
-        return nombre_empleado
+        nombre_base = "resultadoFaltas"
+        extension = "xlsx"
+        nombre_archivo = obtener_nombre_archivo(nombre_base, extension)
+        wb.save(nombre_archivo)
 
-    # Escribir datos en el archivo de Excel
-    row = 2
-    for pin, (dias_no_checados, nombres, dispositivos) in dias_no_checados_por_empleado.items():
-        if dias_no_checados:  # Verificar si hay días no checados
-            # print(f"PIN: {pin}, Dias no checados: {dias_no_checados}, Nombres: {nombres}, Dispositivos: {dispositivos}")
+        messagebox.showinfo("Archivo creado", f"Se ha creado el archivo '{nombre_archivo}' con éxito.")
+    except Exception as e:
+        print("Error", f"Se ha producido un error: {str(e)}")
+        messagebox.showerror("Error", f"Se ha producido un error: {str(e)}")
 
-            # Escribir el PIN, nombre del empleado y dispositivo
-            ws.cell(row=row, column=1).value = int(pin)  # Convertir PIN a entero
-            ws.cell(row=row, column=2).value = buscar_nombre_por_pin(pin)  # Suponiendo que el nombre siempre estará en la primera posición
-            ws.cell(row=row, column=3).value = buscar_dispositivo_por_pin(pin)  # Suponiendo que el dispositivo siempre estará en la primera posición
-
-            # Escribir las fechas en las columnas correspondientes
-            for idx, fecha in enumerate(fechas, start=1):
-                if fecha in dias_no_checados:
-                    ws.cell(row=row, column=3+idx).value = fecha.strftime("%d/%m/%Y")
-                else:
-                    ws.cell(row=row, column=3+idx).value = "A"
-            row += 1
-
-    # ws.delete_cols(ws.max_column)
-
-    # Guardar el archivo de Excel
-    nombre_base = "resultadoFaltas"
-    extension = "xlsx"
-
-    def comparar_archivos(archivo_seleccionado, archivo_todos):
-        # Cargar datos del archivo seleccionado por el usuario
-        with open(archivo_seleccionado, 'r', newline='', encoding='utf-8') as file:
-            csv_reader = csv.DictReader(file)
-            data_seleccionados = list(csv_reader)
-
-        # Cargar datos del archivo todos.csv
-        with open(archivo_todos, 'r', newline='', encoding='utf-8') as file:
-            csv_reader = csv.DictReader(file)
-            data_todos = list(csv_reader)
-
-        # Encontrar empleados faltantes en el archivo seleccionado
-        pins_seleccionados = set(empleado["PIN"] for empleado in data_seleccionados)
-        empleados_faltantes = [empleado for empleado in data_todos if empleado["PIN"] not in pins_seleccionados]
-
-        return empleados_faltantes
-
-    # Llamada a la función para comparar archivos
-    empleados_faltantes = comparar_archivos(archivo_checadores, 'todos.csv')
-
-    # Si hay empleados faltantes, agregarlos a una nueva hoja en el archivo Excel
-    if empleados_faltantes:
-        ws_faltantes = wb.create_sheet(title="Empleados no existentes")
-
-        # Escribir encabezados en la nueva hoja
-        ws_faltantes.append(["PIN", "Nombre de empleado","Dispositivo"])
-
-        # Escribir datos de empleados faltantes en la nueva hoja
-        for empleado in empleados_faltantes:
-            ws_faltantes.append([empleado["PIN"], empleado["Nombre de empleado"], empleado["Dispositivo"]])
-
-    # nombre_archivo = f"{os.path.splitext(archivo_checadores)[0]}.xlsx"
-    # wb.save(nombre_archivo)
-    nombre_archivo = obtener_nombre_archivo(nombre_base, extension)
-    wb.save(nombre_archivo)
-
-    # # Indicar al usuario que se ha creado el archivo
-    # print(f"Se ha creado el archivo '{nombre_archivo}' con éxito.")
-    messagebox.showinfo("Archivo creado", f"Se ha creado el archivo '{nombre_archivo}' con éxito.")
-except Exception as e:
-    messagebox.showerror("Error", f"Se ha producido un error: {str(e)}")
+if __name__ == "__main__":
+    main()
